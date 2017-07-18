bc84b20ee3d9970c3993e6d15b190fb7adac4a61
James Spence <Jamesaspence20@gmail.com>
2017-07-17 22:52:07 -0500

    deleted unecessary middleware binding

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
