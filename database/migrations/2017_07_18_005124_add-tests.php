f7e3bc9acab222f91b9563ad645e5eefa8feef32
Trent <trentkrogers@gmail.com>
2017-07-17 20:02:29 -0500

    Migration for speed tests

---
+++ database/migrations/2017_07_18_005124_add-tests.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('device_id', false, true);
+            $table->double('download_speed');
+            $table->double('upload_speed');
+            $table->timestamps;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
