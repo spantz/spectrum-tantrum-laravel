08262b2ba0394aaaaaf40f1d9df3aa26b0c90ac9
Trent <trentkrogers@gmail.com>
2017-07-17 23:29:42 -0500

    tests model

---
+++ app/Models/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    protected $fillable = ['device_id', 'upload_speed', 'download_speed'];
+}
