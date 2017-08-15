ac96c30565c1dd1227336c78f5e4908dd9bef969
Theo Syslack <theosyslack@gmail.com>
2017-08-14 21:04:54 -0500

    Updated Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+<div class="u-flex center">
+    <div class="card">
+        <div class="header">
+            <h1 class="title">Login</h1>
+        </div>
+        <form method="POST" action="{{ route('login') }}">
+            <div class="body">
+                {{ csrf_field() }}
+                <label>
+                    <h2>E-mail Address</h2>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                       
+                    @endif
+                </label>
+                <label>
+                    <h2>Password</h2>
+                    <input id="password" type="password" class="form-control" name="password" required>
+                    
+                    @if ($errors->has('password'))
+                        <div class="error">
+                            <strong>{{ $errors->first('password') }}</strong>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                            </div>
-                        </div>
-                    </form>
+                    @endif
+                </label>
+                <div class="u-flex">
+                    <h3>Remember Me</h3> <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
                 </div>
             </div>
-        </div>
+            <div class="footer">
+                <button href="{{ url('register') }}" class="button">Register</button>
+                <button type="submit" class="button primary">Login</button>
+            </div>    
+        </form>      
     </div>
 </div>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,18 @@
 <div class="header">
-    <h1>ISPing</h1>
-        @if (Route::has('login'))
-        {{--  <div>  --}}
-            @if (Auth::check())
-            <div class="u-flex">
-                <a class="button" href="{{ url('/settings') }}">Settings</a>
-                <form action="{{ url('/logout') }}" method="post">
-                    {{ csrf_field() }}
-                     <button type="submit" class="button">Logout</button>
-                </form>
-            </div>
-            @else
-                <a href="{{ url('/login') }}" class="button">
-                    Login
-                </a>
+    <h1 class="title"><a href="{{ url('/') }}">ISPing</a></h1>
+    @if (Route::has('login'))
+        @if (Auth::check())
+        <div class="u-flex">
+            <a class="button" href="{{ url('/settings') }}">Settings</a>
+            <form action="{{ url('/logout') }}" method="post">
+                {{ csrf_field() }}
+                <button type="submit" class="button">Logout</button>
+            </form>
+        </div>
+        @else
+            @if(Request::route()->getName() != 'login')
+                <a href="{{ url('/login') }}" class="button">Login</a>
             @endif
-        {{--  </div>  --}}
+        @endif
     @endif
 </div>
