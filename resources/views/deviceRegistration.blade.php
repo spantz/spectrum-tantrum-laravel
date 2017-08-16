def79973d9d5ebeb646cda9ad40e68860f7e75e8
Theo Syslack <theosyslack@gmail.com>
2017-08-15 19:15:41 -0500

    Improved code styling and spacing utilities

--- resources/views/deviceRegistration.blade.php
+++ resources/views/deviceRegistration.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="setup-instructions u-flex space-between">
-    <div class="card u-good-measure two-column">
+<div class="setup-instructions u-flex space-between no-stretch">
+    <div class="card u-good-measure two-column u-margin-right _5">
         <div class="header red">
             <h2>Let's setup a device!</h2>
         </div>
@@ -21,7 +21,44 @@
             <h2>Instructions</h2>
         </div>
         <div class="body">
+            <h2>Step 1</h2>
             <p>
+                <ul>
+                    <li> 
+                        <p>
+                            <code>Node 7.0.0</code>
+                        </p>
+                    </li>
+                    <li> 
+                        <p>
+                            <code>Yarn v2</code>
+                        </p>
+                    </li>
+                    <li> 
+                        <p>
+                            <code>Node v7</code>
+                        </p>
+                    </li>
+                    <li> 
+                        <p>
+                            <code>Node v7</code>
+                        </p>
+                    </li>
+                </ul>
+            </p>
+            <p>
+                In order to test your internet speed, we'll need to
+                communicate with some computer on your local network.
+                We'd reccomend something like a Raspberry Pi, but as long as you can
+                meet the minimum requirements, you should be good!
+            </p>
+             <p>
+                In order to test your internet speed, we'll need to
+                communicate with some computer on your local network.
+                We'd reccomend something like a Raspberry Pi, but as long as you can
+                meet the minimum requirements, you should be good!
+            </p>
+             <p>
                 In order to test your internet speed, we'll need to
                 communicate with some computer on your local network.
                 We'd reccomend something like a Raspberry Pi, but as long as you can
