1d5a3591934b5ad83fb315630b29b0777e27c71a
James Spence <Jamesaspence20@gmail.com>
2017-09-11 23:54:05 -0500

    PHPdoc, type hinting on ConversionUtil

--- app/Util/ConversionUtil.php
+++ app/Util/ConversionUtil.php
@@ -19,47 +19,107 @@ class ConversionUtil
     const BITS_TO_BYTES_FACTOR = .125;
     const BYTES_TO_BITS_FACTOR = 8;
 
-    public static function convertBitsToBytes($amount): float
+    /**
+     * Converts bits to bytes.
+     * Can be used to convert from Kb -> KB, or Mb -> MB.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertBitsToBytes(float $amount): float
     {
         return self::round($amount * self::BITS_TO_BYTES_FACTOR);
     }
 
-    public static function convertBytesToBits($amount): float
+    /**
+     * Converts bytes to bits.
+     * Can be used to convert from KB -> Kb, or MB -> Mb.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertBytesToBits(float $amount): float
     {
         return self::round($amount * self::BYTES_TO_BITS_FACTOR);
     }
 
-    public static function convertKiloToMega($amount): float
+    /**
+     * Converts kilo to mega.
+     * Can be used to convert from Kb -> Mb, or KB -> MB.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertKiloToMega(float $amount): float
     {
         return self::round($amount * self::KILO_TO_MEGA_FACTOR);
     }
 
-    public static function convertMegaToKilo($amount): float
+    /**
+     * Converts mega to kilo.
+     * Can be used to convert from Mb -> Kb, or MB -> Mb.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertMegaToKilo(float $amount): float
     {
         return self::round($amount * self::MEGA_TO_KILO_FACTOR);
     }
 
-    public static function convertKilobitsToMegabytes($amount): float
+    /**
+     * Converts kilobits to megabytes.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertKilobitsToMegabytes(float $amount): float
     {
         return self::round($amount * (self::KILO_TO_MEGA_FACTOR * self::BITS_TO_BYTES_FACTOR));
     }
 
-    public static function convertMegabytesToKilobits($amount): float
+    /**
+     * Converts megabytes to kilobits.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertMegabytesToKilobits(float $amount): float
     {
         return self::round($amount * (self::MEGA_TO_KILO_FACTOR * self::BYTES_TO_BITS_FACTOR));
     }
 
-    public static function convertMegabitsToKilobytes($amount)
+    /**
+     * Converts megabits to kilobytes.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertMegabitsToKilobytes(float $amount)
     {
         return self::round($amount * (self::MEGA_TO_KILO_FACTOR * self::BITS_TO_BYTES_FACTOR));
     }
 
-    public static function convertKilobytesToMegabits($amount)
+    /**
+     * Converts kilobytes to megabits.
+     *
+     * @param float $amount
+     * @return float
+     */
+    public static function convertKilobytesToMegabits(float $amount)
     {
         return self::round($amount * (self::KILO_TO_MEGA_FACTOR * self::BYTES_TO_BITS_FACTOR));
     }
 
-    public static function round($amount, $precision = 2): float
+    /**
+     * Rounds off our number using the {@link round} method
+     * @see round()
+     *
+     * @param float $amount
+     * @param int $precision
+     * @return float
+     */
+    public static function round(float $amount, int $precision = 2): float
     {
         return round($amount, $precision);
     }
