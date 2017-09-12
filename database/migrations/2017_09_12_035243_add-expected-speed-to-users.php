04b016df9c9bb5ee92a7bdae94c428902dcaabd5
James Spence <Jamesaspence20@gmail.com>
2017-09-11 23:05:21 -0500

    new migration for expected_speed (in kilobytes)

---
+++ database/migrations/2017_09_12_035243_add-expected-speed-to-users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExpectedSpeedToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->float('expected_speed')->default(50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('expected_speed');
+        });
+    }
+}
