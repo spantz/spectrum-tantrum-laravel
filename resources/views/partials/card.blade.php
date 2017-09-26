0f9546ff4c05ff0cfe1d6af78f7d93da75f95b31
Theo Syslack <theosyslack@gmail.com>
2017-09-25 19:23:25 -0500

    Added SpeedGraph component

---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,12 @@
+<div class="card">
+  <div class="header">
+    <div class="title">{{$title}}</div>
+  </div>
+  <div class="body">
+    {{$body}}
+  </div>
+  <div class="footer">
+    {{$footer}}
+  </div>
+  </form>
+</div>
