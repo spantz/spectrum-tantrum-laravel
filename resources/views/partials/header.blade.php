fac9979e9dcc7ac3cec064d93e0c2ded15f56999
Theo Syslack <theosyslack@gmail.com>
2017-09-25 21:36:46 -0500

    Removed settings

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,16 +3,11 @@
     @if (Route::has('login'))
         @if (Auth::check())
         <div class="u-flex">
-            <a class="button" href="{{ url('/settings') }}">Settings</a>
-            {{--  <form action="{{ url('/logout') }}" method="post">  --}}
-                {{--  {{ csrf_field() }}  --}}
-
-                <a href="{{ url('/logout') }}" class="button">Logout</a>
-            {{--  </form>  --}}
+            {{--  <a class="button" href="{{ url('/settings') }}">Settings</a>  --}}
+            <a href="{{ url('/logout') }}" class="button">Logout</a>
         </div>
         @else
         <div class="u-flex flex-end">
-
           @if(Request::route()->getName() != 'login')
               <a href="{{ url('/login') }}" class="button">Login</a>
               <a href="{{ url('/register') }}" class="button">Register</a>
